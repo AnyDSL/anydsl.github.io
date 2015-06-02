7982967f47bb59ad2b0f7190a456a619b475d6d1
MasterCassim <MasterCassim@users.noreply.github.com>
2015-06-02 11:50:44 +0200

    added parsing javascript function

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+function parse(val) {
+    var result = "Not found",
+        tmp = [];
+    var items = location.search.substr(1).split("&");
+    for (var index = 0; index < items.length; index++) {
+        tmp = items[index].split("=");
+        if (tmp[0] === val) result = decodeURIComponent(tmp[1]);
+    }
+    return result;
+}
