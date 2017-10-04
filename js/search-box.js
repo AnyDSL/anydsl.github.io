0977366959c8512542b704f7b26154ead9acba1d
Stefan Lemme <stefan.lemme@dfki.de>
2017-10-04 23:21:20 +0200

    Remove debug output of search box

--- js/search-box.js
+++ js/search-box.js
@@ -19,25 +19,21 @@ function initSearchBox(searchInput, resultsOutput, indexUrl) {
             case 'ArrowDown':
                 var active = resultsOutput.find('li.drawer-list__item--highlight');
                 if (active.get(0)) {
-                    console.log('previous active found', active.get(0));
                     active.removeClass('drawer-list__item--highlight');
                     active = active.next('li.drawer-list__item');
                 } else {
                     active = resultsOutput.find('li.drawer-list__item').first();
                 }
-                console.log(active.get(0));
                 active.addClass('drawer-list__item--highlight');               
                 break;
             case 'ArrowUp':
                 var active = resultsOutput.find('li.drawer-list__item--highlight');
                 if (active.get(0)) {
-                    console.log('previous active found', active.get(0));
                     active.removeClass('drawer-list__item--highlight');
                     active = active.prev('li.drawer-list__item');
                 } else {
                     active = resultsOutput.find('li.drawer-list__item').last();
                 }
-                console.log(active.get(0));
                 active.addClass('drawer-list__item--highlight');               
                 break;
             case 'Enter':
